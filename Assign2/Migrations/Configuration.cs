9c0b623f70044d38010c6648c5c2ca9ea95fce68
Satveer Dhillon <Satveerdhillon01@gmail.com>
2018-10-12 11:30:22 -0500

    final

--- Assign2/Migrations/Configuration.cs
+++ Assign2/Migrations/Configuration.cs
@@ -14,6 +14,20 @@ namespace Assign2.Migrations
 
         protected override void Seed(Assign2.Models.ApplicationDbContext context)
         {
+
+            context.Sale.AddOrUpdate(i => i.Id,
+               new Models.Sale()
+               {
+                   Id = 1,
+                   Date = DateTime.Now,
+               });
+
+            context.Customer.AddOrUpdate(i => i.Id,
+                new Models.Customer(){Id = 1,});
+
+            context.Employee.AddOrUpdate(i => i.Id,
+            new Models.Employee()
+            {Id = 1,});
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
