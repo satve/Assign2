4f85f41f5d13f50e55ea35c98002afd991146808
Satveer Dhillon <Satveerdhillon01@gmail.com>
2018-10-12 09:46:40 -0500

    intial commit

---
+++ Assign2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assign2.Startup))]
+namespace Assign2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
